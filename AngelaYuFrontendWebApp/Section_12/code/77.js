66720b71379e0457363fa622d9fa937e2711caca
Bhupendra Verma <bhupendraverma7388@gmail.com>
2024-04-06 22:55:19 +0530

    done

---
+++ AngelaYuFrontendWebApp/Section_12/code/77.js
@@ -0,0 +1,2 @@
+console.log("Hello World!!");
+alert("hello");//it does not work here. run only on DOM
---
+++ AngelaYuFrontendWebApp/Section_12/code/88.js
@@ -0,0 +1,7 @@
+var a = "3"
+var b = "8"
+var x=a;
+a=b;
+b=x;
+console.log(a)
+console.log(b)
